81e431c0c499e35ebffd596978a2a0af62bdfd94
Rutong <l-rutong@unext.jp>
2024-10-20 03:35:13 +0900

    add legacy build support for channel-copier

--- plugins/channel-copier/cmake/legacy.cmake
+++ plugins/channel-copier/cmake/legacy.cmake
@@ -1,48 +1,22 @@
-project(coreaudio-encoder)
+project(channel-copier)
+
+add_library(channel-copier MODULE)
+add_library(OBS::channel-copier ALIAS channel-copier)
+
+target_sources(channel-copier PRIVATE channel-copier.c)
+
+target_link_libraries(
+ channel-copier
+  PRIVATE
+    OBS::libobs
+    $<$<PLATFORM_ID:Windows>:OBS::w32-pthreads>
+)
 
 if(OS_WINDOWS)
-  option(ENABLE_COREAUDIO_ENCODER "Enable building with CoreAudio encoder (Windows)" ON)
-  if(NOT ENABLE_COREAUDIO_ENCODER)
-    obs_status(DISABLED "coreaudio-encoder")
-    return()
-  endif()
+  configure_file(cmake/windows/obs-module.rc.in channel-copier.rc)
+  target_sources(channel-copier PRIVATE channel-copier.rc)
 endif()
 
-add_library(coreaudio-encoder MODULE)
-add_library(OBS::coreaudio-encoder ALIAS coreaudio-encoder)
+set_target_properties(channel-copier PROPERTIES FOLDER "plugins")
 
-target_sources(coreaudio-encoder PRIVATE encoder.cpp)
-
-set_target_properties(
-  coreaudio-encoder
-  PROPERTIES OUTPUT_NAME "coreaudio-encoder"
-             FOLDER "plugins"
-             PREFIX "")
-
-target_compile_features(coreaudio-encoder PRIVATE cxx_std_11)
-
-target_link_libraries(coreaudio-encoder PRIVATE OBS::libobs)
-
-if(OS_MACOS)
-  find_library(COREFOUNDATION CoreFoundation)
-  find_library(COREAUDIO CoreAudio)
-  find_library(AUDIOTOOLBOX AudioToolbox)
-
-  mark_as_advanced(AUDIOTOOLBOX COREAUDIO COREFOUNDATION)
-
-  target_link_libraries(coreaudio-encoder PRIVATE ${COREFOUNDATION} ${COREAUDIO} ${AUDIOTOOLBOX})
-
-elseif(OS_WINDOWS)
-  if(MINGW)
-    set_source_files_properties(encoder.cpp PROPERTIES COMPILE_FLAGS -Wno-multichar)
-  endif()
-
-  target_compile_definitions(coreaudio-encoder PRIVATE UNICODE _UNICODE)
-
-  set(MODULE_DESCRIPTION "OBS CoreAudio encoder")
-  configure_file(${CMAKE_SOURCE_DIR}/cmake/bundle/windows/obs-module.rc.in coreaudio-encoder.rc)
-
-  target_sources(coreaudio-encoder PRIVATE coreaudio-encoder.rc windows-imports.h)
-endif()
-
-setup_plugin_target(coreaudio-encoder)
+setup_plugin_target(channel-copier)
